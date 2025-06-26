e46224f1e4c8aef0805ac1ef3b757c83991c3030
abdisetiakawan <bedikadiryt@gmail.com>
2025-06-26 23:01:15 +0700

    feat: Add user relationships for subscriptions/addresses
    Adds relationships for subscriptions and addresses to the User model.
    
    This allows easy access to a user's subscriptions and addresses
    through the `subscriptions()` and `addresses()` methods.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -52,5 +53,15 @@ class User extends Authenticatable
     public function isAdmin(): bool
     {
         return $this->role === 'admin';
+    }
+
+    public function subscriptions(): HasMany
+    {
+        return $this->hasMany(Subscription::class);
+    }
+
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(UserAddress::class);
     }
 }
